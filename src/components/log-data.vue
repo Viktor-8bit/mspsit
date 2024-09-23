4442a7297eda66f83b41a6dcbb689afc356df065
Кожевников Иван <64777603+Viktor-8bit@users.noreply.github.com>
2024-09-23 15:11:34 +0700

    log list page
    added page with all logs

--- src/components/log-data.vue
+++ src/components/log-data.vue
@@ -13,15 +13,40 @@
 </script>
 
 <template>
-
     <div v-for="log in logs.getLogs" :key="log.id">
-        <div>{{log}}</div>
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-2 ip-text"> {{ log['ip'] }}</div>
+                <div class="col">{{ log['data'] }}</div>
+                <div class="col">{{ log['status'] }}</div>
+            </div>
+        </div>
+        <div class="query-container ">
+            <div class="row">
+                <div class="col-2 col-ms-3">{{ log['user_agent'] }}</div>
+                <div class="col query">{{ log['query'] }}</div>
+            </div>
+            <button type="button" class="btn btn-success mt-2">Подробнее</button>
+        </div>
     </div>
-
 </template>
 
 <style>
+    .query {
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
 
+    .query-container {
+        padding: 0% 0.6% 5% 0.6%;
+        height: 20%;
+    }
+
+    .ip-text {
+        color: #42ac50;
+        font-family: "Trebuchet MS";
+        font-weight: 600;
+    }
 
 </style>
 
--- src/stores/log.js
+++ src/stores/log.js
@@ -6,24 +6,18 @@ import axios from "axios";
 
 export const useLogsStore = defineStore('serverLogs', {
     state: () => ({
-        /** @type {{ id: int, ip: string, data: string, status: int, query: string, user_agent: string  }[]} */
         logs: []
     }),
     getters: {
+        /** @type {{ id: int, ip: string, data: string, status: int, query: string, user_agent: string  }[]} */
         getLogs(state) { return state.logs },
-        fuck() { return "fuck" }
     },
     actions: {
         async get_logs() {
-            console.log('foijsfgoijrieojiopjiuh')
-            var tem_logs = await axios.get("http://127.0.0.1:5000/api/get_all_logs", (result) => {
-                return result.data;
+            await axios.get("http://127.0.0.1:5000/api/get_all_logs").then((result) => {
+                this.logs =result.data
             })
-            self.logs = tem_logs
         }
-    },
-    result: {
-        
     }
 })
 
